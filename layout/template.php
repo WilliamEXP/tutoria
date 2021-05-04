89940ff221fae73aadb0872deda64b057830e203
Marco <marco.com17@gmail.com>
2021-05-03 21:16:59 -0500

    De vuesta al menú inicial

--- layout/template.php
+++ layout/template.php
@@ -13,13 +13,13 @@
     <div class="container">
         <div class="menu">
         
-        <h3 class = "txt-center">Índice</h3>
+        <h3>Índice</h3>
         <li><a href="index.php" id = "index">Directorio</a></li>
         <li><a href="introduccion.php" id = "introduccion">Introducción</a></li>
     
         <li><a href="operacion.html">Operación del programa</a></li>
-        <li ><a href = "aspectos.html" class="sub">Aspectos fundamentales de operación</a></li>
-        <li><a href="tuto_gru.html" class="sub">Tutoria grupal</a></li>
+        <li class="sub"><a href = "aspectos.html">Aspectos fundamentales de operación</a></li>
+        <li class="sub"><a href="tuto_gru.html">Tutoria grupal</a></li>
         <li class="sub"><a href="tuto_indi.html">Tutoría individual</a></li>
         <li class="sub"><a href="comite.html">Comité de Tutorías</a></li>
         <li class="sub"><a href="contexto.html">Contexto y operación de la evaluación</a></li>
@@ -32,16 +32,6 @@
     
         <li><a href="guia.html">Guia Para el Tutor</a></li>
         <li class="sub"><a href="sesion1.html">Semestre 1</a></li>
-        <li class="sub"><a href="s8-semana14.php">Semestre 7 semana 14</a></li>
-        <li class="sub"><a href="s8-semana15.php">Semestre 7 semana 15</a></li>
-        <li class="sub"><a href="s8-semana16.php">Semestre 7 semana 16</a></li>
-        <li class="sub"><a href="s8-semana1.php">Semestre 8 semana 1</a></li>
-        <li class="sub"><a href="s8-semana2.php">Semestre 8 semana 2</a></li>
-        <li class="sub"><a href="s8-semana3.php">Semestre 8 semana 3</a></li>
-        <li class="sub"><a href="s8-semana4.php">Semestre 8 semana 4</a></li>
-        <li class="sub"><a href="s8-semana5.php">Semestre 8 semana 5</a></li>
-        <li class="sub"><a href="s8-semana6.php">Semestre 8 semana 6</a></li>
-        <li class="sub"><a href="s8-semana7.php">Semestre 8 semana 7</a></li>
        
        </div>
        
