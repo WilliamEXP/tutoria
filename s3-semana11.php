564738db7e2fb37124c29994c3ca87c5e8877d7b
Brenda <brendaCancheku@hotmail.com>
2021-05-03 23:23:00 -0500

    Añadí el semestre 3 semana 3 a la 11

---
+++ s3-semana11.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorias</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css?4.0" media="all">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+</head>
+
+<body>
+    <?php include('layout/semestre3.php'); ?>
+
+    <div class="main">
+        <div class="text txt-justify">
+            <h1>Semestre 3</h1>
+
+            <h2>Semana 11 | ELABORACIÓN DE CUADROS SINÓPTICOS</h2>
+
+            <h3>Descripción</h3>
+            <p>Elaboración de un cuadro sinóptico.</p>
+
+            <h3>Pasos a seguir:</h3>
+            <p>El Tutor deberá:</p> 
+            <ol>
+                <li>Identificar a los alumnos del grupo que tengan la capacidad de liderazgo.</li>
+                <li>Formar los equipos con al menos un alumno con capacidad de liderazgo.</li>
+                <li>Presentará a los tutorados las características principales de un cuadro sinóptico.</li>
+                <li>Presentar ejemplos de un cuadro sinóptico bien hechos y mal hechos (disponible en el material de apoyo).</li>
+                <li>Mostrar el uso de programas en línea para el diseño de un cuadro sinóptico como <a href="http://www.powtoon.com">www.powtoon.com</a> y <a href="http://www.lucidchart.com">www.lucidchart.com</a></li>
+                <li>Asignar los temas a trabajar. Supervisar que los equipos estén trabajando en la elaboración del cuadro sinóptico.</li>
+                <li>Elegir algún cuadro sinóptico para que se presente al grupo y se hagan las retroalimentaciones de los mismos.</li>
+                <li>Dar una retroalimentación a cada una de los cuadros sinópticos.</li>
+            </ol>
+            <p> <strong><U>Nota:</U></strong> El tutor puede apoyarse de videos disponible en material.</p>
+        </div>
+
+        <a href="s3-semana10.php" class="atras"><</a>
+        <a href="s3-semana12.php" class="siguiente">></a>
+    </div>
+</body>
+
+</html>
