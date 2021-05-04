1d1c731391ba8e2d9c00055e864446ed1a0f72ce
David Dzul <davidf.dzul@gmail.com>
2021-05-04 10:21:40 -0500

    ultimas semanas

---
+++ s8-semana11.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorias</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css?4.0" media="all">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+</head>
+
+<body>
+    <?php include('layout/semestre8.php'); ?>
+
+    <div class="main">
+        <div class="text txt-justify">
+            <h2>Semana 11 | LA ENTREVISTA LABORAL EN INGLÉS</h2>
+
+            <h3>Descripción</h3>
+            <p>Establecer los aspectos importantes a considerar cuando se acude a una entrevista, laboral. Se recomienda al tutor trabajar de manera conjunta con la coordinación de idiomas.</p>
+
+            <h3>Pasos a seguir:</h3>
+
+            <ul>
+                <li>El Tutor retoma la idea de las técnicas de hablar en público.</li>
+                <li>El Tutor organiza la actividad para que todos los estudiantes del grupo pasen en plenaria y pongan en práctica las técnicas aprendidas.</li>
+                <li>Se reflexiona sobre el ejercicio realizado, el tutor retroalimenta la sesión.</li>
+            </ul>
+        </div>
+
+        <a href="index.php" class="atras"><</a>
+        <a href="operacion.php" class="siguiente">></a>
+    </div>
+</body>
+
+</html>
