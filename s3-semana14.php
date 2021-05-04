99ef1efc1fd10a53691386b2f5a48842a78a73ee
heberth <david10.uhau@gmail.com>
2021-05-03 19:48:45 -0500

    Semestre 3, semanas 14, 15 y 16 - Estilo para tablas

---
+++ s3-semana14.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorias</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css?4.0" media="all">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+</head>
+
+<body>
+    
+    <?php include('layout/template.php'); ?>
+
+    <div class="main">
+        <div class="text txt-justify">
+            <h2>Semana 14 | EVALUACIÓN DE LOS TUTORADOS</h2>
+
+            <h3>Descripción</h3>
+            <p>Introducción del Programa Institucional de Tutoría (PIT) del semestre y organización de la foto grupal con el Tutor.</p>
+
+            <h3>Pasos a seguir:</h3>
+
+            <ul>
+                <li>El Tutor recaba las evidencias de los tutorados relacionadas con el desarrollo de las habilidades establecidas en el formato de evaluación al desempeño de la actividad complementaria y llena el formato correspondiente.</li>
+                <li>El Tutor llena el formato de tutoría individual correspondiente, con la firma del tutorado.</li>
+            </ul>
+
+            <p><b><u>NOTA: </u></b>Para el desarrollo de las actividades de la semana 15 UN DÍA POR MI TEC, el tutor deberá definir con su Jefatura de Carrera las necesidades, insumos, implementos de trabajo y seguridad necesarios para llevarlas a cabo.</p>
+        </div>
+
+        <a href="s3-semana13.php" class="atras"><</a>
+        <a href="s3-semana15.php" class="siguiente">></a>
+    </div>
+</body>
+
+</html>
