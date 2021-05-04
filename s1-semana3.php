23c21b5acb48484de541b4045ed362eff791bb21
Nicolas-Lalo <eduardo.17070011@itsmotul.edu.mx>
2021-05-03 23:04:39 -0500

    Parte del contenido del semestre 1 listo

---
+++ s1-semana3.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorias</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css?4.0" media="all">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    
+</head>
+
+<body>
+    <?php include('layout/semestre1.php'); ?>
+
+    <div class="main">
+        <div class="text txt-justify">
+
+            <h2>Semana 3 | FUNCIONAMIENTO DEL ITSMOTUL (parte II)</h2>
+
+            <h3>Descripción</h3>
+            <p>Presentación de las actividades complementarias y 
+            eventos académicos dentro de la Institución.</p>
+
+            <h3>Pasos a seguir:</h3>
+
+            <ol type = "1">
+                <li>1.	El Tutor proyecta la presentación 
+                “Funcionamiento del Tec Parte II” disponible en
+                el material con el objeto de conocer los
+                departamentos y áreas con los que tendrá
+                contacto el estudiante para solicitar diferentes 
+                servicios y sus respectivos responsables.</li>
+                <li>De ser posible, dar un paseo por las 
+                instalaciones.</li>
+            </ol>
+        </div>
+
+        <a href="s1-semana2.php" class="atras"><</a>
+        <a href="s1-semana4.php" class="siguiente">></a>
+    </div>
+</body>
+
+</html>
---
+++ s1-semana7.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorias</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css?4.0" media="all">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+    <?php include('layout/semestre1.php'); ?>
+
+    <div class="main">
+        <div class="text txt-justify">
+    
+            <h2>Semana 7 | LA LÍNEA DE LA VIDA</h2>
+
+            <h3>Descripción</h3>
+            <p>Reflexionar acerca de la identificación de patrones de
+            conducta y etapas de la vida del estudiante.</p>
+
+            <h3>Pasos a seguir:</h3>
+
+            <ol type = "1">
+                <li>El Tutor presentará ante los alumnos un 
+                ejemplo de la línea de la vida disponible en el
+                 material. 
+                </li>
+
+                <img src="img/linea-vida.png" class="img">
+                
+                <li>El Tutor expone las siguientes instrucciones.</li>
+                
+                <p>2.1 Grafique la curva de tu vida en el espacio.
+                    Dibuje una línea que represente su vida
+                    desde la infancia hasta el día de hoy,
+                    mostrando los puntos altos y bajos. El eje
+                    vertical representa el nivel de felicidad, 
+                    satisfacción y realización. El eje horizontal 
+                    representa tu edad al momento de los 
+                    eventos significativos y los puntos de
+                    inflexión.</p>
+
+                <p>2.2.	Marque en la línea central las personas,
+                eventos y acciones importantes que
+                tuvieron lugar. Si ha pasado por un cambio
+                dramático importante, deberá representar
+                esto comenzando una nueva línea. Por
+                ejemplo, el comienzo de una nueva curva “S”.</p>
+                
+                <p>2.3.	Identifique las principales etapas de su 
+                vida dándole un título a cada “capítulo”.
+                Por ejemplo, 6-12 años: “Necesidad de
+                amor y reconocimiento”, 13-18 años: 
+                “Búsqueda de identidad y aventura”, 19-
+                30: “Búsqueda de riqueza e importancia”,
+                etcétera.</p>
+                
+                <p>2.4.	Identifique los patrones principales que 
+                aparecen en las diferentes etapas o 
+                “capítulos”. Lo más probable es que éstos
+                se repitan también en los siguientes 
+                “capítulos”, a menos que sean 
+                comprendidos. </p>
+
+            </ol>
+        </div>
+        
+        <a href="s1-semana6.php" class="atras"><</a>
+        <a href="s1-semana8.php" class="siguiente">></a>
+    </div>
+</body>
+
+</html>
