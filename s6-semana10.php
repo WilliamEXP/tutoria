e1a20876fd233b0b31c233c0fe95e4d15d92499d
Marco <marco.com17@gmail.com>
2021-05-04 00:24:45 -0500

    Lsitos

---
+++ s6-semana10.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Tutorias</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css?4.0" media="all" >
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+</head>
+<body>
+  <?php include('layout/semestre6.php'); ?>
+  
+  <div class="main">
+      <div class = "text txt-justify">
+	<h2>Semana 10 | LIDERAZGO</h2>
+	
+	<h3>Descripción</h3>
+	<p>Reflexión sobre la importancia de ser un buen líder.</p>
+
+	<h3>Pasos a seguir:</h3>
+	<ol>
+	  <li>Se proyecta el video “El Puente” disponible en el material de apoyo en la semana 7.</li>
+	  <li>Después del video, el Tutor reflexionará con los estudiantes que tipo de conductas se consideran positivas y cuáles negativas y porqué. La idea es que el Tutor ayude a descubrir algunas de las características de un buen liderazgo.</li>
+	  <li>Posteriormente el Tutor proyectará el video “Liderazgo efectivo”.</li>
+	  <li>Finalmente proyectará el video “Liderazgo y Motivación”.</li>
+	  <li>Se hace la retroalimentación intercambiando ideas.</li>
+	</ol>
+
+	<a href="index.php" class ="atras"><</a>
+	<a href="operacion.php" class ="siguiente">></a>
+      </div>
+  </div>
+
+</body>
+</html>
---
+++ s7-semana2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Tutorias</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css?4.0" media="all" >
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+</head>
+<body>
+  <?php include('layout/semestre7.php'); ?>
+  
+  <div class="main">
+      <div class = "text txt-justify">
+	<h2>Semana 2 | REDES SOCIALES</h2>
+	
+	<h3>Descripción</h3>
+	<p>Conocer los elementos de las redes sociales que los empleadores utilizan para discriminar entre potenciales empleados.</p>
+
+	<h3>Pasos a seguir:</h3>
+	<ol>
+	  <li>El Tutor proveerá dos artículos que exponen los elementos que los empleadores revisan en los perfiles de las redes sociales de los candidatos a un puesto de trabajo (disponibles en el material).</li>
+	  <li>El Tutor llevará a cabo una discusión sobre los aspectos que los estudiantes encuentran interesantes.</li>
+	  <li>Se armarán parejas para revisar sus perfiles personales en una red personal para identificar aspectos positivos y negativos que pudiera afectarles en su vida laboral.</li>
+	  <li>Discutir en plenaria las medidas a considerar a partir de ese momento con respecto a sus redes sociales.</li>
+	</ol>
+
+	<a href="index.php" class ="atras"><</a>
+	<a href="operacion.php" class ="siguiente">></a>
+      </div>
+  </div>
+
+</body>
+</html>
